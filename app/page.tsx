986062a4ea235b71b9b94587da5a73505d3faf5f
amir.gh <amir.962@yahoo.com>
2023-10-15 00:04:46 +0330

    add font to project(layout.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      
+      <div className='font-notosans text-3xl'>amir</div>
+      <div className='text-3xl'>amir</div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: "class",
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkestBlue: "rgb(4, 54, 74)",
+        darkBlue: "rgb(23, 107, 135)",
+        skyBlue: "rgb(100, 204, 197)",
+        whiteBlue: "rgb(218, 255, 251)"
+      },
+      fontFamily: {
+        notosans: ['var(--font-noto-sans)'],
       },
     },
   },
