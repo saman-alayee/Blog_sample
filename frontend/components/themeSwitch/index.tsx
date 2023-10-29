0ef2c6e365aa7698c693fd25c0c6adbdd571842c
soheilyr <yazdanyar.dev@gmail.com>
2023-10-29 15:59:27 +0330

    rewrite switch theme btn

--- frontend/components/themeSwitch/index.tsx
+++ frontend/components/themeSwitch/index.tsx
@@ -1,31 +1,17 @@
 "use client";
-import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import { BsMoon } from "react-icons/bs"
-import { BiSun } from "react-icons/bi"
-
+import { BsMoon } from "react-icons/bs";
+import { BiSun } from "react-icons/bi";
 
 export const ThemeSwitcher = () => {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-
-    if (!mounted) {
-        return null;
-    }
-
-
-    return (
-        <button
-            className={`flex justify-center items-center md:w-7 md:h-7 w-9 h-9 rounded-md hover:scale-110 active:scale-100 duration-200 dark:bg-slate-200 dark:text-black bg-[#212933] text-white`}
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-            {theme === "light" ? <BsMoon /> : <BiSun />}
-        </button>
-    );
+  return (
+    <button
+      className={`flex justify-center items-center md:w-7 md:h-7 w-9 h-9 rounded-md hover:scale-110 active:scale-100 duration-200 dark:bg-slate-200 dark:text-black bg-[#212933] text-white`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "light" ? <BiSun /> : <BsMoon />}
+    </button>
+  );
 };
