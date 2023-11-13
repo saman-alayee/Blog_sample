ff5dd370b8a06ea1a9809553851b39ccd8bc8075
amir.gh <amir.962@yahoo.com>
2023-11-14 00:02:01 +0330

    change footer info

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,7 +4,7 @@ import NavItem from '../navbar/NavItem'
 import SocialIcons from './SocialIcons'
 const Footer = () => {
   return (
-    <div className='w-full flex flex-col justify-center md:items-center md:flex-row  md:justify-around space-y-6 md:space-y-0 p-8  mt-8 md:mt-0'>
+    <div className='w-full flex flex-col justify-center md:items-center md:flex-row  md:justify-around space-y-6 md:space-y-0 p-4 mt-[6vh] md:mt-0'>
       <BlogLogo />
       <NavItem />
       <SocialIcons />
